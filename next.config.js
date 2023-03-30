b45892cd977a91df5a7c58dca6456fa5da36a62b
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-03-30 22:58:36 +0530

    Made the footer using daisyui and the form with email

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "www.google.com",
+      "assets.vogue.com",
+      "m.media-amazon.com",
+      "images.unsplash.com",
+    ],
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '@/components/Navbar'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -14,9 +14,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
+      <main>
         <Navbar />
+        <Footer />
       </main>
     </>
-  )
+  );
 }
