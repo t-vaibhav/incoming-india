9d6a1d86f402bf9a66aac8080295f09bf506823b
t-vaibhav <118555370+t-vaibhav@users.noreply.github.com>
2023-04-07 15:25:42 +0530

    optimization

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,10 +14,10 @@ export default function Hero() {
                     Your browser does not support HTML5 video.
                 </video>
             </div>
-            <div className='bg-black/50 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center text-white text-center'>
+            <div className='bg-black/50 px-2 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center text-white text-center'>
                 <div className=' max-w-[1200px] space-y-4'>
                     <h1 className='text-4xl md:text-6xl font-semibold font-poppins tracking-wide hero-shadow '>INCOMING INDIA</h1>
-                    <p className='text-2xl font-poppins hero-shadow'>Discover The Magic Of India With Your Ultimate Travel Partner!</p>
+                    <p className='md:text-2xl text-xl  font-poppins hero-shadow'>Discover The Magic Of India With Your Ultimate Travel Partner!</p>
                 </div>
             </div>
             <div className='absolute bottom-0 text-white left-0 right-0 flex items-center justify-center'>
--- components/Section.jsx
+++ components/Section.jsx
@@ -10,44 +10,47 @@ export default function Section() {
   const langs = Data;
 
   return (
-    <div className="flex flex-col max-w-full" id="sectionDiv">
+    <div className="flex flex-col max-w-full overflow-x-hidden mx-auto" id="sectionDiv" >
       <h1 className="text-white text-4xl font-poppins font-semibold md:text-5xl text-center mt-8 mb-0">
-        WonderLust India
+        WanderLust India
       </h1>
-      <div className="h-[500px] text-white flex items-center justify-center mt-4 mb-24 max-w-full">
-        <div className="max-w-5xl my-0">
-          <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={(cur) => setActive(cur.realIndex)}
-            loop={true}
-            centeredSlides={true}
-            speed={800}
-            autoplay={{ delay: 2000 }}
-            modules={[Autoplay]}
-          >
-            {langs.map((Lang, i) => (
-              <SwiperSlide key={i}>
-                <div className="h-96 w-72 flex no-border bg-gradient-to-tr from-blue-100/20 to-[#dedd81]/10 justify-evenly rounded-t-xl">
-                  <div className={`w-72 no-border cursor-pointer border-b-4 rounded-xl h-56 object-cover text-center self-end rounded-t-xl duration-500 delay-100  ${active === i && "h-96"} `}>
-                    <div className="relative text-5xl w-full text-white flex  h-1/2 ">
-                      <Image
-                        alt=""
-                        width={288}
-                        height={192}
-                        src={Lang.url}
-                        className=" text-5xl grid place-items-center h-full w-full object-cover rounded-t-xl"
-                      />
+      <div className="flex justify-center ">
+
+        <div className="h-[500px] text-white flex items-center justify-center mt-4 mb-24 max-w-full mx-auto">
+          <div className="max-w-5xl my-0">
+            <Swiper
+              spaceBetween={55}
+              slidesPerView={3}
+              onSlideChange={(cur) => setActive(cur.realIndex)}
+              loop={true}
+              centeredSlides={true}
+              speed={800}
+              autoplay={{ delay: 2000 }}
+              modules={[Autoplay]}
+            >
+              {langs.map((Lang, i) => (
+                <SwiperSlide key={i}>
+                  <div className="h-96 w-72 flex no-border bg-gradient-to-tr from-blue-100/20 to-[#dedd81]/10 justify-evenly rounded-t-xl">
+                    <div className={`w-72 no-border cursor-pointer border-b-4 rounded-xl h-56 object-cover text-center self-end rounded-t-xl duration-500 delay-100  ${active === i && "h-96"} `}>
+                      <div className="relative text-5xl w-full text-white flex  h-1/2 ">
+                        <Image
+                          alt=""
+                          width={288}
+                          height={192}
+                          src={Lang.url}
+                          className=" text-5xl grid place-items-center h-full w-full object-cover rounded-t-xl"
+                        />
+                      </div>
+                      <h2 className={`text-2xl  mt-2 font-semibold font-josefin ${active === i && "text-white"}`}>
+                        {Lang.name}
+                      </h2>
+                      <p className={`para mt-3 font-oswald text-lg   ${active === i && "h-96"}`}>{Lang.des}</p>
                     </div>
-                    <h2 className={`text-2xl  mt-2 font-semibold font-josefin ${active === i && "text-white"}`}>
-                      {Lang.name}
-                    </h2>
-                    <p className={`para mt-3 font-oswald text-lg   ${active === i && "h-96"}`}>{Lang.des}</p>
                   </div>
-                </div>
-              </SwiperSlide>
-            ))}{" "}
-          </Swiper>
+                </SwiperSlide>
+              ))}{" "}
+            </Swiper>
+          </div>
         </div>
       </div>
     </div >
