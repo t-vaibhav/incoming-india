9735fe3a8168eaaa7c43936863016e83d4322309
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-04-14 22:59:58 +0530

    Presences page with details

---
+++ components/Data.jsx
@@ -0,0 +1,87 @@
+const Data = [
+  {
+    img: "https://i.ibb.co/JRKNbnB/c-rayban-CIXo-Fys3gsw-unsplash-min.jpg",
+    title: "JAIPUR",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/vxMQ2Ds/satyaprakash-kumawat-ky1d-IWCBis-unsplash-min.jpg",
+    title: "BENGULURU",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural.",
+  },
+  {
+    img: "https://i.ibb.co/zrHyp7q/abhishek-chakraborty-d2e-VURGcqjc-unsplash-min.jpg",
+    title: "KOLKATA",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/BgRGFWj/boudhayan-bardhan-x-ABa7ibv-H70-unsplash-min.jpg",
+    title: "SILIGURI",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/FgR1D3C/govind-krishnan-pw-Pc-Yzy-JTt-Y-unsplash-min.jpg",
+    title: "TRIVANDRUM",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/2qnLY6X/ashwini-chaudhary-monty-ps1-XIFGEBRg-unsplash-min.jpg",
+    title: "LEH & LADAKH",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/rbgBvJ5/divya-agrawal-qa8-Vhqv-JGIo-unsplash-min.jpg",
+    title: "MANALI",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/fQPrw0T/surya-teja-o-IXJ839p55k-unsplash-min.jpg",
+    title: "SRINAGAR",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/qR5DWJd/chirag-jain-vp-OWk-XUbko8-unsplash-min.jpg",
+    title: "AMRITSAR",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/YbsyPGx/sylwia-bartyzel-e-U4pip-U-8-HA-unsplash-min.jpg",
+    title: "AGRA",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/wpdfgkB/pratish-srivastava-Iu06-T6z2v-RE-unsplash-min.jpg",
+    title: "VARANASI",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/XtFkrZC/abhijeet-parmar-dir-HVh-TQXgg-unsplash-min.jpg",
+    title: "KHAJURAHO",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/bd0TQkj/aashutosh-paliwal-FC4n-n-CVUw-unsplash-min.jpg",
+    title: "UDAIPUR",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+  {
+    img: "https://i.ibb.co/rp0ZsD8/sumit-sourav-e-SRtx-Pd9q1c-unsplash-min.jpg",
+    title: "GOA",
+    location:
+      "Bhutan, the land of the Thunder Dragon, offers a unique blend of natural. ",
+  },
+];
+export default Data;
